b713c10883d31fd352cb1a3321055fba3300b437
ITevgeniydav2022 <defox2022@gmail.com>
2023-03-01 00:13:44 +0300

    Start commit

---
+++ src/main/java/ru/team2/lookingforhouse/LookingForHouseApplication.java
@@ -0,0 +1,13 @@
+package ru.team2.lookingforhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingForHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookingForHouseApplication.class, args);
+    }
+
+}
