9c72e498617471bde94c996ea4f20ed34321b409
Tatyana Bogodvid <mamchic-tatyana@mail.ru>
2023-03-06 10:59:21 +0300

    Созданы классы BotConfig, BotInitializer, TelegramBot. Создана кнопка меню.

---
+++ src/main/java/ru/team2/lookingforhouse/service/TelegramBot.java
@@ -0,0 +1,49 @@
+package ru.team2.lookingforhouse.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.team2.lookingforhouse.config.BotConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "Приветствует пользователя"));
+        listOfCommands.add(new BotCommand("/info", "Выводит информацию о приюте"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
