ed784126883574727354d66ce060275bdd7300c6
gerar <gerarr999@yandex.ru>
2023-03-16 20:26:39 +0300

    Dodelal

--- src/main/java/ru/team2/lookingforhouse/model/ReportData.java
+++ src/main/java/ru/team2/lookingforhouse/model/ReportData.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 
